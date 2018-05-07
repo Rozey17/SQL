be26fe17ac094c26ef4ffd0e31425fe6f577b9a9
ferrad.fm@gmail.com <ferrad.fm@gmail.com>
2018-05-07 20:33:45 +0200

    delete and search buttons implemented

--- SQL/Form1.cs
+++ SQL/Form1.cs
@@ -12,9 +12,30 @@ namespace SQL
 {
     public partial class Form1 : Form
     {
+        private List<Model> customer = new List<Model>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void UpDateBinding()
+        {
+            listBox1.DataSource = customer;
+            listBox1.DisplayMember = "Display";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataAccess data = new DataAccess();
+            customer = data.GetPeople(textBox1.Text);
+            UpDateBinding();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataAccess data = new DataAccess();
+            data.Delete(textBox1.Text);
+            UpDateBinding();
         }
     }
 }
